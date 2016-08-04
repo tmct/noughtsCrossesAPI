aa57b43ef05a79a7fdfb009cb5c91dab0b1752a6
Tom McTiernan <Thomas.McTiernan@softwire.com>
2016-08-04 17:44:31 +0100

    Return game 1 data

--- routes/games.js
+++ routes/games.js
@@ -5,15 +5,23 @@ var database = new Firebase(process.env.FIREBASE_DB);
 
 /* GET grids listing. */
 router.get('/', function(req, res, next) {
-    function doStuff() {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ a: 'HOORAY' }));
+    function getGame1() {
+        return database.child('GameInProgress1').once('value').then(processGameAndReturn, returnError);
     }
-    function stuffIsBroken() {
+    function processGameAndReturn(snapshot) {
+        var game = snapshot.val();
+        if (!game) {
+            return returnError('Game does not exist');
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ a: 'OH NO' }));
+        res.send(JSON.stringify({ a: game }));
     }
-    return database.authWithCustomToken(process.env.FIREBASE_KEY).then(doStuff, stuffIsBroken);
+    function returnError(reason) {
+        var givenReason = reason || 'Mystery error';
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).send(JSON.stringify({ "Error": reason }));
+    }
+    return database.authWithCustomToken(process.env.FIREBASE_KEY).then(getGame1, returnError);
 });
 
 module.exports = router;
