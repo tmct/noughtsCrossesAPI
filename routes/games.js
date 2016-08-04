871a011b7ebee2e2c423ab0b15fbc246c9fe01d5
Tom McTiernan <Thomas.McTiernan@softwire.com>
2016-08-04 16:27:29 +0100

    Talk to firebase

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+var database = new Firebase(process.env.FIREBASE_DB);
 
 /* GET grids listing. */
 router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }));
+    function doStuff() {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ a: 1 }));
+    }
+    function stuffIsBroken() {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ a: 'OH NO' }));
+    }
+    return database.authWithCustomToken(process.env.FIREBASE_KEY).then(doStuff, stuffIsBroken);
 });
 
 module.exports = router;
