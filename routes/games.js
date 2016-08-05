b25ceef5cbce97d2787a5f4d285cb880b4e6c6d8
Tom McTiernan <Thomas.McTiernan@softwire.com>
2016-08-05 11:27:49 +0100

    Get ready to create game

--- routes/games.js
+++ routes/games.js
@@ -87,6 +87,42 @@ router.post('/:gameId/move', function(req, res, next) {
         .catch(returnError)
 });
 
+router.post('/:gameId', function(req, res, next) {
+    function createGame() {
+        var gameId = parseInt(req.params.gameId);
+        if (!gameId) {
+            returnError('Invalid game ID');
+        }
+        var gameName = 'GameInProgress' + gameId;
+        return database.child(gameName).once('value')
+            .then(processGameAndDoMoreStuff)
+            .catch(returnError);
+    }
+    function processGameAndDoMoreStuff(snapshot) {
+        var game = snapshot.val();
+        if (game) {
+            return returnError('Game already exists!');
+        }
+        var newGrid = [
+            ["-","-","-"],
+            ["-","-","-"],
+            ["-","-","-"]
+        ];
+
+        res.setHeader('Content-Type', 'application/json');
+        var successMessage = "Would create grid: " + JSON.stringify(newGrid);
+        res.send(JSON.stringify({"Success": successMessage}));
+    }
+    function returnError(reason) {
+        var givenReason = reason || 'Mystery error';
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).send(JSON.stringify({ "Error": reason }));
+    }
+    return database.authWithCustomToken(process.env.FIREBASE_KEY)
+        .then(createGame)
+        .catch(returnError)
+});
+
 function processGame(game) {
     var rows = [game.row1, game.row2, game.row3];
     return {
